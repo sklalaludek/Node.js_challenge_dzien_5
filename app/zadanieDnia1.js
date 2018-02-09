debd6653076bb1748f28a8a6be0126545df3008b
sklalaludek <pattiegolis@gmail.com>
2018-02-09 17:36:34 +0100

    zadanie dnia 5

--- app/zadanieDnia1.js
+++ app/zadanieDnia1.js
@@ -1,1 +1,25 @@
 //Twój kod
+const express = require('express');
+const app = express();
+let yes = 0,
+    no = 0;
+
+app.use(express.static('./public/zadanieDnia/'));
+
+app.get('/vote/:answer', (req, res) => {
+    const answer = req.params.answer;
+    if (answer === 'yes') {
+        yes++;
+    } else if (answer === 'no') {
+        no++;
+    }
+    res.send('Dziękujemy za głos!');
+});
+
+app.get('/votes/check', (req, res) => {
+    res.send(`Głosy za: ${yes}, przeciwko: ${no}`);
+});
+
+app.listen(3000, () => {
+    console.log('Serwer uruchomiony na porcie 3000');
+});
