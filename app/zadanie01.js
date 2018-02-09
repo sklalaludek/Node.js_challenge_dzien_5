b09332ecceb7b1d6de2e196d355413b5e3314b8c
sklalaludek <pattiegolis@gmail.com>
2018-02-09 17:13:24 +0100

    ex 1-2

--- app/zadanie01.js
+++ app/zadanie01.js
@@ -1,1 +1,11 @@
 //Twój kod
+const express = require('express');
+const app = express();
+
+app.get('/:num1/:num2',(req, res) => {
+    res.send((Number(req.params.num1) + Number(req.params.num2)).toString());
+});
+
+app.listen(3000, () => {
+    console.log('Serwer uruchomiony na porcie 3000');
+});
--- app/zadanie02.js
+++ app/zadanie02.js
@@ -1,1 +1,25 @@
 //Twój kod
+const express = require('express');
+const app = express();
+let name = '';
+
+app.get('/name/set/:name',(req,res) => {
+    name = req.params.name;
+    res.send(`Witaj ${name}`);
+});
+
+app.get('/name/show',(req,res) => {
+    res.send(`Podane wcześniej imię ${name}`);
+});
+
+app.get('/name/check', (req,res) => {
+    if (name !== '') {
+        res.send('Imię zostało zapisane w programie!')
+    } else {
+        res.send('Imię nie zostało zapisane w programie!');
+    }
+});
+
+app.listen(3000, () => {
+    console.log('Serwer uruchomiony na porcie 3000');
+});
